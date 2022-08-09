6a7eff7efa992c85de6e5f95324136d6668b763c
AsdWaring <a.s.d.waring@gmail.com>
2022-08-09 11:13:28 -0700

    progressed on grid

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Etch_Sketch
+
+August 9th
+Okay I am struggling with understanding how to create a grid in CSS so I looked up some code for me to break apart so I can understand the concepts.
+
+It's like learning how to create a button that initiates a function all over again.
+
+I literally had to break the entire code down step by step so I understood how it worked together.
+
+I am really enjoying this, don't let my comments towards a struggle imply that I don't see that struggle as an oppertunity for growth.
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // don't mind me I have no idea what I'm doing yet.
+// reboot. Deleted all code for a start over
+//
 
 const container = document.getElementById("container");
 
-function creategrid(x) {
-  rows = x;
-  columns = x;
-  console.log(rows, columns);
+function makeRows(input) {
+  container.style.setProperty("--grid-rows", input);
+  container.style.setProperty("--grid-cols", input);
+  for (c = 0; c < input * input; c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  }
 }
 
-creategrid(5);
+makeRows(6);
