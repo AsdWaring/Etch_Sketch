0ce562b91838ef678d67a653c321c3c15042f46c
AsdWaring <a.s.d.waring@gmail.com>
2022-08-08 20:00:20 -0700

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+// don't mind me I have no idea what I'm doing yet.
+
+const container = document.getElementById("container");
+
+function creategrid(x) {
+  rows = x;
+  columns = x;
+  console.log(rows, columns);
+}
+
+creategrid(5);
